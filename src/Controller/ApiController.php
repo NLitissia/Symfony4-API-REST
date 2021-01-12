5044226701ba9ec4e932cdc927ae69c0d2e250d9
NLitissia <litissia.negaa@gmail.com>
2021-01-12 16:47:08 +0100

    recuperer departement de chaque région

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiController extends AbstractController
 {
@@ -23,8 +24,24 @@ class ApiController extends AbstractController
        //$regionObj = $serializer->denormalize($regionTab,'App\Entity\Region[]');
        //dump($regionObj).die();
        $regionObj = $serializer->deserialize($regions,'App\Entity\Region[]','json');
-       return $this->render('api/index.html.twig',[
+       return $this->render('api/region.html.twig',[
           'regions' => $regionObj
        ]);
+    }
+    /**
+     * @Route("region/{id}/departements",name="departementParRegion")
+     */
+    public function regionParDepartement($id,SerializerInterface $serializer){
+        
+        $departements = file_get_contents('https://geo.api.gouv.fr/regions/'. $id .'/departements');
+        $departements = $serializer->decode($departements,'json');
+         
+        return $this->render('api/departement.html.twig',[
+            'departements' => $departements
+        ]);
+
+
+        
+
     }
 }
