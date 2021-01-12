e1ea090a25cdc1b6f4270ca6fc97ab42677b7b55
NLitissia <litissia.negaa@gmail.com>
2021-01-12 15:18:47 +0100

    Serialiser et afficher les régions

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/listeregions", name="listeregions")
      */
-    public function index(): Response
+    public function index(SerializerInterface $serializer)
     {
        $regions = file_get_contents('https://geo.api.gouv.fr/regions');
-       dump($regions).die();
-        return $this->render('api/index.html.twig');
+       //dump($regions).die();
+       $regionTab = $serializer->decode($regions,'json');
+       //dump($regionTab).die();
+       return $this->render('api/index.html.twig',[
+          'regions' => $regionTab
+       ]);
     }
 }
